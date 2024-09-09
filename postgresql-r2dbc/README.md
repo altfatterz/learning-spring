a50fe11ce0c9e32d12a41e7dd3d9462247440c8d
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2024-09-09 17:25:54 +0200

    postgresql r2dbc try out different config

--- postgresql-r2dbc/README.md
+++ postgresql-r2dbc/README.md
@@ -27,5 +27,34 @@ $ http :8080/customers\?lastName=Doe
         "name": "Jane Doe"
     }
 ]
-
 ```
+
+### Running without Testcontainers
+
+- Start PostgreSQL
+
+```bash
+$ docker compose up -d
+```
+
+- Start the application
+
+```bash
+$ mvn clean package
+$ java -jar target/postgresql-r2dbc-0.0.1-SNAPSHOT.jar \
+--spring.r2dbc.url=r2dbc:postgresql://localhost:<port>/postgres \
+--spring.r2dbc.username=postgres \
+--spring.r2dbc.password=secret
+```
+
+- Add data to database
+```bash
+$ psql -p <port> -U postgres
+insert into customers(firstname, lastname) values ('John', 'Doe');
+insert into customers(firstname, lastname) values ('Jane', 'Doe');
+insert into customers(firstname, lastname) values ('John', 'Wick');
+```
+
+Resources:
+
+https://gokhana.dev/spring-r2dbc-for-reactive-relational-databases-in-reactive-programming/
---
+++ postgresql-r2dbc/src/main/java/com/github/altfatterz/postgresqlr2dbc/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package com.github.altfatterz.postgresqlr2dbc;
+
+//import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+//import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+//import io.r2dbc.spi.ConnectionFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
+
+//@Configuration
+//public class DatabaseConfig extends AbstractR2dbcConfiguration {
+//
+//    @Override
+//    @Bean
+//    public ConnectionFactory connectionFactory() {
+//        return new PostgresqlConnectionFactory(
+//                PostgresqlConnectionConfiguration.builder()
+//                        .host("localhost")
+//                        .database("postgres")
+//                        .port(55579)
+//                        .username("postgres")
+//                        .password("secret")
+//                        .build());
+//    }
+//}
