048f05ea9445181ca7fe000f9f7735556220a3f2
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2024-09-25 13:04:09 +0200

    postgresql jdbc ssl connection

---
+++ postgresql-jdbc-secure/src/main/java/com/github/altfatterz/postgresqljdbcsecure/PostgresqlJdbcSecureApplication.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.postgresqljdbcsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlJdbcSecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresqlJdbcSecureApplication.class, args);
+    }
+
+}
