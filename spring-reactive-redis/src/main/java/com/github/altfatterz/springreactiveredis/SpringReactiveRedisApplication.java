81f88fce7c1c4e89fb2a342bcf596ee0e5fb305e
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2024-09-27 11:38:24 +0200

    redis demo

---
+++ spring-reactive-redis/src/main/java/com/github/altfatterz/springreactiveredis/SpringReactiveRedisApplication.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.springreactiveredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveRedisApplication.class, args);
+    }
+
+}
