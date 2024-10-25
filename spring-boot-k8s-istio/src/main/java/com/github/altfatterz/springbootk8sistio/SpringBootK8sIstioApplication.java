91dde2dd6980a9fd075f0890c34d6b5a7baf8ade
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2024-10-25 09:51:44 +0200

    istio core-dns try out

---
+++ spring-boot-k8s-istio/src/main/java/com/github/altfatterz/springbootk8sistio/SpringBootK8sIstioApplication.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.springbootk8sistio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootK8sIstioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootK8sIstioApplication.class, args);
+    }
+
+}
