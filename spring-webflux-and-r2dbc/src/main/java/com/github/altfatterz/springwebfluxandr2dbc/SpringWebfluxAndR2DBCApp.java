f64ae606f7deb09cd09c5a7e8768c2e77a144523
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2024-12-08 17:47:14 +0100

    add example webflux with jdbc and r2dbc

---
+++ spring-webflux-and-r2dbc/src/main/java/com/github/altfatterz/springwebfluxandr2dbc/SpringWebfluxAndR2DBCApp.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.springwebfluxandr2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxAndR2DBCApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxAndR2DBCApp.class, args);
+    }
+
+}
---
+++ spring-webflux-and-r2dbc/src/test/java/com/github/altfatterz/springwebfluxandr2dbc/SpringWebfluxAndR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.springwebfluxandr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxAndR2dbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
