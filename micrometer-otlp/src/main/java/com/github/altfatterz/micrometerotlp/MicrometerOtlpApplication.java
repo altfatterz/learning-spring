1daa202a343aaac3e4f331d5ee735d651fae9415
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2025-01-21 10:58:51 +0100

    add micrometer otlp demo

---
+++ micrometer-otlp/src/main/java/com/github/altfatterz/micrometerotlp/MicrometerOtlpApplication.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.micrometerotlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerOtlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerOtlpApplication.class, args);
+	}
+
+}
