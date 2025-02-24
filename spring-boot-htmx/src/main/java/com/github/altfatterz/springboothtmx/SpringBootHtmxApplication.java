4588bbb9b8126152479b7dde9fa7a3c1fbbd9bdf
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2025-02-24 08:53:59 +0100

    spring-boot-htmx demo

---
+++ spring-boot-htmx/src/main/java/com/github/altfatterz/springboothtmx/SpringBootHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.springboothtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHtmxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHtmxApplication.class, args);
+    }
+
+}
---
+++ spring-boot-htmx/src/test/java/com/github/altfatterz/springboothtmx/SpringBootHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.altfatterz.springboothtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHtmxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
