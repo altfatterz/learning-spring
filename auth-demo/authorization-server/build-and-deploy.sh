4c085cae89151d24909fbfe2cf3c3a445ac4780a
Zoltan Altfatter <zoltan.altfatter@mimacom.com>
2026-01-30 21:45:30 +0100

    remove the auth-demo project to external project

--- auth-demo/authorization-server/build-and-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mvn clean package spring-boot:build-image
-k3d images import authorization-server:0.0.1-SNAPSHOT -c k3s-default
-docker exec k3d-k3s-default-server-0 crictl images | grep authorization-server
-kubectl apply -f k8s/k8s.yaml
-kubectl rollout restart deployment authorization-server
